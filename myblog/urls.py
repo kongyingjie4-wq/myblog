d128c5ae898512a02b240a799ffd8816576fa7f9
kongyingjie4-wq <kongyingjie4@gmail.com>
2026-03-05 18:26:00 +0800

    fix: redirect root to blog

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView  # 新增这一行
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('', RedirectView.as_view(url='/blog/', permanent=True)), # 让首页自动跳转到 blog
 ]
