5ba26b2d216dc2b91627c0f68492a71feb1239d8
kongyingjie4-wq <kongyingjie4@gmail.com>
2026-03-05 18:39:17 +0800

    fix: add missing admin import and root redirect

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,8 +1,9 @@
+from django.contrib import admin  # 就是少了这一行！
 from django.urls import path, include
-from django.views.generic import RedirectView  # 新增这一行
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('', RedirectView.as_view(url='/blog/', permanent=True)), # 让首页自动跳转到 blog
+    path('', RedirectView.as_view(url='/blog/', permanent=True)),
 ]
